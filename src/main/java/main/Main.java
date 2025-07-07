3a7d9d960bbdaafa9fb7d1ff740825edec1e82e2
Alexei1231 <popowalex4@gmail.com>
2025-07-07 18:34:17 +0200

    prototype, GSON and JSON were added

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,40 @@
 package main;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+public class Main {
+    Scanner sc = new Scanner(System.in);
+
+
+
+    public static void main(String[] args) {
+        String seedUrl = "https://www.booking.cz";//target webpage
+
+        Document doc = retrieveHTML(seedUrl);
+
+        if (doc != null) {
+            System.out.println("HTML successfully retrieved!");
+        }
+    }
+
+    private static Document retrieveHTML(String url) {
+        try {
+            // download HTML document using JSoup's connect class
+            Document doc = Jsoup.connect(url).get();
+
+            // log the HTML content
+            System.out.println(doc.html());
+
+            // return the HTML document
+            return doc;
+        } catch (IOException e) {
+            // handle exceptions
+            System.err.println("Unable to fetch HTML of: " + url);
+        }
+        return null;
     }
 }
